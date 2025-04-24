5f8357fbce6061e64d80222f717066bfb90012cc
Boku7-glitch <sababokuchava2009@gmail.com>
2025-04-24 20:32:39 +0400

    Add blog posting functionality

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
+
+const BLOGS_FILE = 'blogs.json';
 
 const requireAuth = (req, res, next) => {
     if (req.session.user) {
@@ -14,7 +17,26 @@ router.get('/', requireAuth, function(req, res, next) {
 });
 
 router.get('/new', requireAuth, function(req, res, next) {
-    res.render('new_blog');
+    res.render('new_blog', { error: null });
+});
+
+router.post('/new', requireAuth, function(req, res, next) {
+    const { title, content } = req.body;
+
+    if (!title || !content) {
+        res.render("new_blog", { error: "Missing title or content" });
+    }
+
+
+    const blogs = JSON.parse(fs.readFileSync(BLOGS_FILE));
+    // const newBlog = {
+    //     id: String(Date.now()),
+    //     title,
+    //     content,
+    //     author
+    // }
+    console.log(req.session.user.email);
+    res.render('new_blog', { error: null });
 });
 
 module.exports = router;
